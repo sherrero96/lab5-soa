735ff52cf77939e46ac5b158a1d3ed5d7b4fdbf3
sherrero96 <sergio.herrero.barco@mail.com>
2019-12-16 10:16:02 +0100

    max search included

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -26,7 +29,26 @@ public class SearchController {
 
   @RequestMapping(value = "/search")
   @ResponseBody
-  public Object search(@RequestParam("q") String q) {
-    return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+  public Object search(@RequestParam("q") String q,
+                       @RequestParam(value = "max", required = false, defaultValue = "10") int max) {
+
+    // Parse the q if contains <word> max:<Number>
+    if(q.contains("max:")){
+      // Obtain the number between the parameter max
+      String patternMax = "max:";
+      String maxString = q.substring(q.indexOf("max") + patternMax.length());
+      max = Integer.parseInt(maxString);
+      // Delete the max:X from the query
+      q = q.substring(0, q.indexOf("max") - 1);
+    }
+
+    System.out.println("Tweets buscados por <" + q +">, mostrando " + max + " resultados");
+
+    Map<String, Object> headers = new HashMap<>();
+    headers.put("CamelTwitterKeywords", q);
+    headers.put("CamelTwitterCount", max);
+    return producerTemplate.requestBodyAndHeaders("direct:search","", headers);
+
+
   }
 }
